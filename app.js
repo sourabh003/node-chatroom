b4889a33959a731354d50fca3522bc1a47708692
sourabh003 <csourabh003@gmail.com>
2023-06-04 00:16:14 +0530

    css fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 // Middlewares
-app.use(express.static(__dirname));
+app.use(express.static("."));
 
 // Routes
 app.get('/', (req, res) => {
